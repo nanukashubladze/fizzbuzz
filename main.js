8063b84d218e6ffbcb321e83b1c53cbc55e6c54e
nanukashubladze <123475291+nanukashubladze@users.noreply.github.com>
2023-06-18 16:30:59 +0400

    alldone

---
+++ main.js
@@ -0,0 +1,16 @@
+let num = 1;
+
+while (num < 100){
+    if (num % 5 === 0 && num % 3 === 0){
+        console.log("fizzbuzz")
+    } else if (num % 5 === 0){
+        console.log("buzz")
+    } else if(num % 3 === 0){
+        console.log("fizz")
+    } else{
+        console.log(num)
+    }
+
+
+    num ++
+}
